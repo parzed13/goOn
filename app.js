402bceca1c91b1a93d1e8c0e11cc8223cb612282
Paris Babaie <parr@Pariss-MacBook-Pro.local>
2018-03-15 21:29:23 -0500

    different path handling simple

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const koa = require('koa');
+const logger = require('koa-logger');
 const app = new koa();
 let userCount = 0;
+app.use(logger()); // loger middle ware : note that middlewares are sequential and they can modify the result which would be input of the next middleware 
 app.use(async ctx => {
-	userCount++;
-  ctx.body = 'We have had ' + userCount + ' visits!\n';
+	if (ctx.url == '/reset'){
+		userCount = 0;
+		ctx.body = `Parr had ${userCount} visits!\n`;
+	}else if (ctx.url == '/view'){
+		ctx.body = `Parr had ${userCount} visits!\n`;
+	}else if (ctx.url == '/increment'){
+		userCount ++;
+		ctx.body = `Parr had ${userCount} visits!\n`;
+	}
+
+	console.log(` mwthod: ${ctx.method} `);
+	console.log(` url: ${ctx.url} `);
+  	 // `` for string operations
 });
+
 app.listen(5000);
+
+
+//async folan => {
+///bahman;
+//}
